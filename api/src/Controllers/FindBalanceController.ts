8d6983e82e2185205280502fe31e3b7b36958648
crishard <crislantorres9@gmail.com>
2023-02-18 21:32:24 -0300

    Criando types para utilizar o id do usuário via requisição padrão

--- api/src/Controllers/FindBalanceController.ts
+++ api/src/Controllers/FindBalanceController.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express";
 
 export class FindBalanceController {
     async handle(req: Request, res: Response) {
-      
+        const findBalance = new FindBalance();
+        const { userId } = req;
+        
     }
 }
