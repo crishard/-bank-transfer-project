7fe09dd0777fb5ca1dc4c14f09677b90c6d368b6
crishard <crislantorres9@gmail.com>
2023-02-18 21:27:19 -0300

    realizando a requisição para verificar se o usuário existe

--- api/src/modules/useCases/Account/findBalance/FindBalance.ts
+++ api/src/modules/useCases/Account/findBalance/FindBalance.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "@prisma/client";
+import { prisma } from "../../../../dataBase/prismaClient";
 
 interface IFindBalance{
     userId: string;
@@ -8,6 +8,12 @@ interface IFindBalance{
 export class FindBalance {
     async execute({ userId }: IFindBalance) {
   
-      
+        const findUser = await prisma.users.findFirst({
+            where: {
+              id: {
+                equals: userId
+              }
+            }
+          })
     }
   }
