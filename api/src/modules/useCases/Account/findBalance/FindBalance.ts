6fffcf10ca9c51dce2aa12d03b7add5f8559f595
crishard <crislantorres9@gmail.com>
2023-02-18 21:28:06 -0300

    realizando a requisição do balanço

--- api/src/modules/useCases/Account/findBalance/FindBalance.ts
+++ api/src/modules/useCases/Account/findBalance/FindBalance.ts
@@ -1,19 +1,31 @@
 import { prisma } from "../../../../dataBase/prismaClient";
 
-interface IFindBalance{
+interface IFindBalance {
     userId: string;
 }
 
 
 export class FindBalance {
     async execute({ userId }: IFindBalance) {
-  
+
         const findUser = await prisma.users.findFirst({
             where: {
-              id: {
-                equals: userId
-              }
+                id: {
+                    equals: userId
+                }
             }
-          })
+        });
+        if (!findUser) {
+            return new Error("Permissão negada!")
+        } else {
+            const findBalance = await prisma.accounts.findMany({
+                where: {
+                    id: {
+                        equals: findUser.accountId
+                    }
+                }
+            });
+            return findBalance;
+        }
     }
-  }
+}
