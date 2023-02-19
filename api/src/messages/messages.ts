81307d5c47fb411e4c6710a3a7fb9deaf0c186be
crishard <crislantorres9@gmail.com>
2023-02-18 21:33:45 -0300

    Criando mensagens padrẽs

--- api/src/messages/messages.ts
+++ api/src/messages/messages.ts
@@ -5,3 +5,39 @@ export const internalError = {
 export const permissionDenied = {
     message: "Você não tem permissão para esta ação"
 }
+
+export const userTokenIsInvalid = {
+    message: 'Token inexistente ou inválido!'
+}
+
+export const userShortName = {
+    message: "Nome de usuário muito curto"
+}
+
+export const userExist = {
+    message: "Nome de usuário já cadastrado"
+}
+
+export const userNotExist = {
+    message: "Usuário não cadastrado"
+}
+
+export const shortPassword = {
+    message: "Senha muito curta"
+}
+
+export const invalidPassword = {
+    message: "Sua senha deve ter ao menos uma letra um número e uma letra maiúscula!"
+}
+
+export const noMovement = {
+    message: "Nenhuma movimentação realiza com sua conta!"
+}
+
+export const emptyFilters = {
+    message: "Nenhum filtro selecionado!"
+}
+
+export const transactionError = {
+    message: "Erro de transação, por favor, informar corretamente os dados!"
+}
