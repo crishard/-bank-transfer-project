e21677f0396a94da433d11c847a2593f074fdce0
crishard <crislantorres9@gmail.com>
2023-02-18 21:31:01 -0300

    Criando escopo do controller

---
+++ api/src/Controllers/FindBalanceController.ts
@@ -0,0 +1,8 @@
+import { FindBalance } from "../modules/useCases/Account/findBalance/FindBalance";
+import { Request, Response } from "express";
+
+export class FindBalanceController {
+    async handle(req: Request, res: Response) {
+      
+    }
+}
