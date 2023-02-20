37d53abce6ba150f746877b3a3a59b9ab12e4a66
crishard <crislantorres9@gmail.com>
2023-02-20 18:04:47 -0300

    atualizando rotas

--- api/src/routers.ts
+++ api/src/routers.ts
@@ -1,4 +1,34 @@
 import { Router } from "express";
 
-export const routes = Router();
+// imports
+import { FindTransactionsController } from "./Controllers/FindTransacionController";
+import { CreateUserController } from "./Controllers/CreateUserController";
+import { FindBalanceController } from "./Controllers/FindBalanceController";
+import { FiltersTransactionsController } from "./Controllers/FilterTransactionsController";
+import { CreateTransactionController } from "./Controllers/CreateTransactionController";
+import { FindUserController } from "./Controllers/FindUserController";
+import { LoginUserController } from "./Controllers/LoginController";
+import { checkUserAuthenticate } from "./modules/useCases/Users/checkUser/checkUser";
 
+
+const createUserController = new CreateUserController();
+const findBalanceController = new FindBalanceController();
+const findTransactionsController = new FindTransactionsController();
+const loginUserController = new LoginUserController();
+const findFilterTransactionsController = new FiltersTransactionsController();
+const findUserController = new FindUserController();
+const createTransactionController = new CreateTransactionController();
+
+const routes = Router();
+
+
+routes.post("/create_user", createUserController.handle);
+routes.get("/balance", checkUserAuthenticate, findBalanceController.handle);
+routes.post("/login", loginUserController.handle);
+routes.get("/transactions", checkUserAuthenticate, findTransactionsController.handle);
+routes.get("/filters_transactions", checkUserAuthenticate, findFilterTransactionsController.handle)
+routes.post("/create_transaction", checkUserAuthenticate, createTransactionController.handle);
+routes.get("/user", checkUserAuthenticate, findUserController.handle);
+
+
+export default routes;
