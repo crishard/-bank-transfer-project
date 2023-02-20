0cffc035bae8d9c5e25902134973b0639765f769
crishard <crislantorres9@gmail.com>
2023-02-20 18:05:32 -0300

    criando projeto react com vite, e realizando injeção de dependências

---
+++ bankTranfer/src/App.tsx
@@ -0,0 +1,12 @@
+import { Form } from "./components/Form/Form"
+import "./styles/global.css"
+
+
+export function App() {
+ 
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  )
+}
