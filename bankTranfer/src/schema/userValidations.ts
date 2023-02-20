5bd79e0a87cc6b08ef06de3407c73011622eb30a
crishard <crislantorres9@gmail.com>
2023-02-20 18:06:54 -0300

    Criando validação de usuários com yup

---
+++ bankTranfer/src/schema/userValidations.ts
@@ -0,0 +1,14 @@
+import * as Yup from 'yup';
+
+export const UserValidate = Yup.object().shape({
+    username: Yup.string()
+        .min(3, 'Nome de usuário muito curto')
+        .required('O campo "Nome do usuário" é obrigatório.'),
+    password: Yup.string()
+        .required('O campo "Senha" é obrigatório.')
+        .min(8, 'Precisa conter pelo menos 8 caracteres.')
+        .matches(
+            /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{7,})\S$/,
+            'Senha precisa conter pelo menos uma letra maiúscula, uma minúscula, e um número.'
+        ),
+});
