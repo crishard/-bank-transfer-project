8230d9b60dcf8ada525bfb2efabfb6cd749b726d
crishard <crislantorres9@gmail.com>
2023-02-20 18:04:35 -0300

    criando função para alterar balanços

---
+++ api/src/modules/helpers/updateBalance.ts
@@ -0,0 +1,39 @@
+import { prisma } from "../../dataBase/prismaClient";
+import {transactionError} from "../../messages/messages"
+
+export async function updateBalance(value: number, userAccountId: string, debited: boolean) {
+
+    const accountExist = await prisma.accounts.findFirst({
+        where: {
+            id: {
+                equals: userAccountId
+            }
+        }
+    });
+    
+    if (accountExist?.balance != null) {
+        if (debited) {
+            const countDebited = await prisma.accounts.update({
+                where: {
+                    id: userAccountId
+                },
+                data: {
+                    balance: accountExist.balance - value
+                }
+            })
+            return countDebited;
+        } else {
+            const countCredited = await prisma.accounts.update({
+                where: {
+                    id: userAccountId
+                },
+                data: {
+                    balance: accountExist.balance + value
+                }
+            });
+            return countCredited;
+        }
+    } else{
+        return new Error(transactionError.message);
+    }
+}
