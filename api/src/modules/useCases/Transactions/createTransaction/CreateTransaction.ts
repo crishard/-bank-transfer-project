57c2c22c8a7ff4c074a30289454f128cf52d61a7
crishard <crislantorres9@gmail.com>
2023-02-20 18:02:14 -0300

    criando função de trnasação entre usuários na api

---
+++ api/src/modules/useCases/Transactions/createTransaction/CreateTransaction.ts
@@ -0,0 +1,69 @@
+import { prisma } from "../../../../dataBase/prismaClient";
+import { checkBalance } from "../../../helpers/checkBalance";
+import { updateBalance } from "../../../helpers/updateBalance";
+
+interface ICreateTransaction {
+    userCashIn: string;
+    value: number;
+    userId: string;
+}
+
+export class CreateTransaction {
+    async execute({ userCashIn, value, userId }: ICreateTransaction) {
+
+        //salvando data do creatAt sem considerar horário
+        const creatAt = new Date().setHours(0, 0, 0, 0)
+        const creatAtDateWithoutHours = new Date(creatAt);
+
+        const userCashInExist = await prisma.users.findUnique({
+            where: {
+                username: userCashIn
+            }
+        })
+
+        const findUserCashOut = await prisma.users.findFirst({
+            where: {
+                id: {
+                    equals: userId
+                }
+            }
+        });
+
+        if (!findUserCashOut) {
+            return new Error("Sua seção expirou!");
+        }
+        else {
+
+            if (value <= 0) {
+                return new Error("Selecione um valor para transação maior que 0!")
+            } else if (!userCashInExist) {
+                return new Error("O usuário selecionado não existe")
+            } else {
+
+                const checkBalanceEnough = await checkBalance(userId, value);
+
+                if (userId == userCashIn) {
+                    return new Error("Você não pode realizar uma transação para sua própria conta!")
+                }
+
+                else if (checkBalanceEnough == false) {
+                    return new Error("Você não tem saldo suficiente para realizar esta transação, por favor verifique o seu saldo!")
+                }
+
+                else {
+                    const transaction = await prisma.transactions.create({
+                        data: {
+                            creatAt: creatAtDateWithoutHours,
+                            value: value,
+                            debitedAccountId: findUserCashOut.accountId,
+                            creditedAccountId: userCashInExist.accountId
+                        }
+                    });
+                    const debitedBalance = await updateBalance(value, findUserCashOut.accountId, true)
+                    const creditedBalance = await updateBalance(value, userCashInExist.accountId, false)
+                    return transaction;
+                }
+            }
+        }
+    }
+}
