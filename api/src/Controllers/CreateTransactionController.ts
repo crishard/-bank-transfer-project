697420fb8f1541c34dc604fd36156066e4e9400f
crishard <crislantorres9@gmail.com>
2023-02-20 18:02:34 -0300

    criando controller de transação entre usuários na ap

---
+++ api/src/Controllers/CreateTransactionController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { CreateTransaction } from "../modules/useCases/Transactions/createTransaction/CreateTransaction";
+
+export class CreateTransactionController {
+    async handle(req: Request, res: Response) {
+        const { userCashIn, value } = req.body;
+        const {userId} = req;
+        const createTransaction = new CreateTransaction();
+        const resultado = await createTransaction.execute({
+            userId,
+            userCashIn,
+            value
+        });
+        if (resultado instanceof Error) {
+            return res.status(400).json(resultado.message)
+        }
+        else if (resultado) {
+            return res.status(200).json(resultado);
+        }
+        return res.json("Erro no servidor");
+    }
+}
