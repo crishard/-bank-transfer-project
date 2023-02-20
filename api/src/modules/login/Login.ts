9f49404d279a735908f4781115036c2ab5c781ce
crishard <crislantorres9@gmail.com>
2023-02-20 18:01:42 -0300

    criando função de login na api

---
+++ api/src/modules/login/Login.ts
@@ -0,0 +1,36 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { prisma } from "../../dataBase/prismaClient";
+
+interface ILoginUser {
+  username: string;
+  password: string;
+}
+
+export class LoginUser {
+  async execute({ username, password }: ILoginUser) {
+    //Receber a senha e password
+
+    // Verificar Cliente Existe
+    const user = await prisma.users.findFirst({
+      where: { username },
+    });
+    if (!user) {
+      return new Error("Usuário não cadastrado");
+    }
+    //Verificar se a senha está correta
+    const compararSenha = await compare(password, user.password);
+
+    if (!compararSenha) {
+      return new Error("Senha invalida");
+    }
+
+    //Gerar umm token
+    const token = sign({ username }, "chavesecreta", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return { token: token };
+  }
+}
