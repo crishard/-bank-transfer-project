dea7243c7a0a1c4a108b20795ee5bed6504c25dc
crishard <crislantorres9@gmail.com>
2023-02-21 21:38:39 -0300

    criando botão de formulários da apliação

--- bankTranfer/src/components/Form/buttonForm/ButtonForm.tsx
+++ bankTranfer/src/components/Form/buttonForm/ButtonForm.tsx
@@ -1,10 +1,11 @@
+import "./ButtonForm.css"
+
 interface IButtonProps{
     text: string;
 }
 
-
 export function ButtonForm(props: IButtonProps){
     return(
-        <button type="submit" className="bg-indigo-600 py-2.5 px-4 text-sm font-medium text-white" >{props.text}</button>
+        <button type="submit" className="button-form bg-indigo-600 py-2.5 px-4 text-sm font-medium text-white" >{props.text}</button>
     );
 }
