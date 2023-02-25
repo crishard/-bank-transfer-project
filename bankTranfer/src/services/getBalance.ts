1b8bc3430e48a84db1aef61c5381395c4102b198
crishard <crislantorres9@gmail.com>
2023-02-24 21:37:46 -0300

    criando requisição para visualização do balanço da conta

---
+++ bankTranfer/src/services/getBalance.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { api } from "../providers/api";
+
+
+export function getBalance<T = unknown>() {
+
+    const token = localStorage.getItem('token')?.replace(/^"(.+(?="$))"$/, '$1');
+
+    const [data, setData] = useState<T | null>(null)
+
+    useEffect(() => {
+        api
+            .get("balance", {
+                headers: { Authorization: `Bearer ${token}` }
+            }
+            ).then(response => setData(response.data));
+    }, []);
+    return { data }
+};
+
