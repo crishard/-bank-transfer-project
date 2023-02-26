9d56b5a5d357c67a683c47de31e3fa29b06a3e79
crishard <crislantorres9@gmail.com>
2023-02-25 21:37:06 -0300

    criando função login e requisição para dados do usuário

---
+++ bankTranfer/src/services/login.ts
@@ -0,0 +1,10 @@
+import { api } from "../providers/api";
+
+export const login = async (username: string, password: string) => {
+    const response = await api
+        .post("login", {
+            username,
+            password,
+        });
+    localStorage.setItem("token", JSON.stringify(response.data));
+};
---
+++ bankTranfer/src/services/profile.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { api } from "../providers/api";
+
+
+export function profileData<T = unknown>() {
+    const token = localStorage.getItem('token')?.replace(/^"(.+(?="$))"$/, '$1');
+
+    const [data, setData] = useState<T | null>(null)
+
+    useEffect(() => {
+        api
+            .get("user", {
+                headers: { Authorization: `Bearer ${token}` }
+            }
+            ).then(response => setData(response.data));
+    }, []);
+    return { data }
+};
+
