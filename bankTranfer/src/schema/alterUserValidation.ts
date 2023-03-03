7332481915f402f1f046a3616c8c3eb701b95f50
crishard <crislantorres9@gmail.com>
2023-03-02 21:41:30 -0300

    criando validação com yup para update dos usuáriose criação de transação

---
+++ bankTranfer/src/schema/alterUserValidation.ts
@@ -0,0 +1,22 @@
+import * as Yup from 'yup';
+
+export const alterUSerValidation = Yup.object({
+      username: Yup.string()
+            .min(3, 'Nome de usuário muito curto')
+            .required('O campo "Nome do usuário" é obrigatório.'),
+      password: Yup.string()
+            .required('O campo "Senha" é obrigatório.')
+            .min(8, 'Precisa conter pelo menos 8 caracteres.')
+            .matches(
+                  /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{7,})\S$/,
+                  'Tente adicionar números e letras maiúsculas.'
+            ),
+      passwordConfirmation: Yup.string()
+            .required('O campo "Senha" é obrigatório.')
+            .min(8, 'Precisa conter pelo menos 8 caracteres.')
+            .matches(
+                  /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{7,})\S$/,
+                  'Tente adicionar números e letras maiúsculas.'
+            )
+            .oneOf([Yup.ref('password'),], 'A confirmação de senha não confere!')
+});
---
+++ bankTranfer/src/schema/transactionValidate.ts
@@ -0,0 +1,16 @@
+import * as Yup from 'yup';
+
+export const transactionValidate = Yup.object().shape({
+    userCashIn: Yup.string().min(3, 'Nome de usuário muito curto!')
+    .required('O campo usuário é obrigatório!'),
+    value: Yup.number()
+        .min(1, 'O valor da transação tem que ser de pelo menos R$: 1.00')
+        .required('O campo "Valor" é obrigatório.'),
+    password: Yup.string()
+    .required('O campo "Senha" é obrigatório.')
+    .min(8, 'Precisa conter pelo menos 8 caracteres.')
+    .matches(
+          /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{7,})\S$/,
+          'Tente adicionar números e letras maiúsculas.'
+    )
+});
