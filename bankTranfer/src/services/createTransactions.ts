6d77ad2cdeb1d299d09f3de0a3399d25de852005
crishard <crislantorres9@gmail.com>
2023-03-06 21:39:47 -0300

    criando requisição de realização de tranferência

---
+++ bankTranfer/src/services/createTransactions.ts
@@ -0,0 +1,24 @@
+
+import { api } from "../providers/api";
+
+export const createTransaction = async (value: number, userCashIn: string, password: string) => {
+
+    const token = localStorage.getItem('token')?.replace(/^"(.+(?="$))"$/, '$1');
+
+    const headerAutorization = {
+        headers: { Authorization: `Bearer ${token}` }
+    }
+
+    console.log(value)
+    const response = await api
+    .post("create_transaction", {
+        value,
+        password,
+        userCashIn,
+    }, headerAutorization
+    ).then(response => (response.data));
+
+
+    return {response}
+};
+
