22732f871cc0227aa44238450f54ce27c2a4b369
crishard <crislantorres9@gmail.com>
2023-03-09 21:45:20 -0300

    Organizando arquivo

---
+++ bankTranfer/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import { Form } from "../components/Form/Form";
+
+export function LoginPage(){
+    return(
+        <main>
+            <Form register={false} titleHeader="Login" optionHeader="um cadastro" />
+        </main>
+    )
+}
--- bankTranfer/src/pages/login/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { Form } from "../../components/Form/Form";
-
-export function LoginPage(){
-    return(
-        <main>
-            <Form register={false} titleHeader="Login" optionHeader="um cadastro"/>
-        </main>
-    )
-}
