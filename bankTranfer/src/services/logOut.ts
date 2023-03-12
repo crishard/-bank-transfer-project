1bd66f6e7100dda8819bc51d4d112254ff039189
crishard <crislantorres9@gmail.com>
2023-03-12 10:43:44 -0300

    correção na função register

--- bankTranfer/src/services/logOut.ts
+++
@@ -1,8 +0,0 @@
-import { useNavigate } from "react-router-dom";
-
-export function logOut() {
-    const navigate = useNavigate()
-
-    localStorage.removeItem("token");
-    navigate("/login");
-};
--- bankTranfer/src/services/register.ts
+++ bankTranfer/src/services/register.ts
@@ -3,15 +3,9 @@ import { api } from "../providers/api";
 export const userRegister = async (username: string, password: string) => {
 
     console.log(username, password)
-    const response = await api.post("create_user", {
+    await api.post("create_user", {
         username: username,
         password: password,
-    }).then((res) => {
-        const { token } = res.data;
-    },
-    )
-    console.log(response);
-    return response;
-
+    })
 };
 
