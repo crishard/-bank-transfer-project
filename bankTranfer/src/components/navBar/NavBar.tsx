847a02e117555849d6fb3bb08b909955ec3526e1
crishard <crislantorres9@gmail.com>
2023-03-13 10:44:30 -0300

    criando logout para colocar no navBar

--- bankTranfer/src/components/navBar/NavBar.tsx
+++ bankTranfer/src/components/navBar/NavBar.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom"
-import { FaUserCircle } from "react-icons/fa"
-import "./NavBar.css"
+import { LogoutButton } from "./LogOutButton";
+
+import "./NavBar.css";
+
 export function NavBar() {
+
 
     return (
         <nav className="navbar">
             <Link className="text-xl font-bold cursor-auto" to="/">Bank Transfer</Link>
-            <Link title="Sair" to="/login"><FaUserCircle className="icon_perfil" /></Link>
+            <LogoutButton />
         </nav>
     )
 }
