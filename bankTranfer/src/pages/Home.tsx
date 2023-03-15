5da07509ede07e356770735fe5594a7b5a25167c
crishard <crislantorres9@gmail.com>
2023-03-15 10:47:34 -0300

    Atualizando Home

--- bankTranfer/src/pages/Home.tsx
+++ bankTranfer/src/pages/Home.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+import { PersonalInformation } from "../components/home/PersonalInformations";
+import { Transaction } from "../components/home/transactions/Transaction";
+import { Loader } from "../components/loader/Loader";
 import { NavBar } from "../components/navBar/NavBar";
 
-export function Home(){
-    return(
-        <div>
-          <NavBar/>
-        </div>
-    )
+export function Home() {
+
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 3000);
+  }, []);
+
+
+  return (
+    <>
+
+      {isLoaded ?
+        <>
+          <NavBar />
+          <main className="px-12">
+            <PersonalInformation />
+            <Transaction />
+          </main>
+        </>
+        : <Loader />}
+
+    </>
+  );
 }
