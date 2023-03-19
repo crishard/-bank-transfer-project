99449d41f8946301f8dd8af3cbff215a6743250e
crishard <crislantorres9@gmail.com>
2023-03-19 10:06:34 -0300

    Atualização da home e navbar

--- bankTranfer/src/components/navBar/NavBar.tsx
+++ bankTranfer/src/components/navBar/NavBar.tsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom"
 import { LogoutButton } from "./LogOutButton";
+import { profileData } from "../../services/profile";
 
 import "./NavBar.css";
 
-export function NavBar() {
+type Repositories = {
+    id: string;
+    username: string;
+    password: string;
+}
 
+export function NavBar() {
+    const { data } = profileData<Repositories>();
 
     return (
         <nav className="navbar">
-            <Link className="text-xl font-bold cursor-auto" to="/">Bank Transfer</Link>
+            <Link className="text-2xl font-bold cursor-auto" to="/">Olá, {data?.username}</Link>
             <LogoutButton />
         </nav>
     )
--- bankTranfer/src/pages/Home.tsx
+++ bankTranfer/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { PersonalInformation } from "../components/home/PersonalInformations";
 import { Transaction } from "../components/home/transactions/Transaction";
 import { Loader } from "../components/loader/Loader";
 import { NavBar } from "../components/navBar/NavBar";
@@ -22,7 +21,6 @@ export function Home() {
         <>
           <NavBar />
           <main className="px-12">
-            <PersonalInformation />
             <Transaction />
           </main>
         </>
