ab51634117651696a701839ab565990f8ef34281
crishard <crislantorres9@gmail.com>
2023-03-29 09:00:35 -0300

    style: mensagem popup

--- bankTransfer/src/components/poPup/PoPup.tsx
+++ bankTransfer/src/components/poPup/PoPup.tsx
@@ -1,6 +1,6 @@
 export function PoPup(props: {text: string}){
     return(
-        <div className="absolute right-px top-px px-16 py-4 bg-green-500 text-white text-xl font-medium">
+        <div className="absolute right-1 top-16 px-16 py-4 bg-[#ccd91a] text-white text-xl font-medium">
             <p>{props.text}</p>
         </div>
     )
