13d6704c49fe38548a2345d791900238428581f3
crishard <crislantorres9@gmail.com>
2023-03-30 16:08:26 -0300

    refactoring: button form

--- bankTransfer/src/components/Form/buttonForm/ButtonForm.tsx
+++ bankTransfer/src/components/Form/buttonForm/ButtonForm.tsx
@@ -6,7 +6,7 @@ interface IButtonProps {
 
 export function ButtonForm(props: IButtonProps) {
     return (
-        <button type="submit" className="button-form" >
+        <button type="submit" className="button-form bg-indigo-600" >
             {props.text}
         </button>
     );
