fbe53bf143e42699b9824999ca552a689a755841
crishard <crislantorres9@gmail.com>
2023-03-30 13:16:16 -0300

    adicionadno novas variáveis de mensagens

--- api/src/messages/messages.ts
+++ api/src/messages/messages.ts
@@ -22,12 +22,8 @@ export const userNotExist = {
     message: "Usuário não cadastrado"
 }
 
-export const shortPassword = {
-    message: "Senha muito curta"
-}
-
 export const invalidPassword = {
-    message: "Sua senha deve ter ao menos uma letra um número e uma letra maiúscula!"
+    message: "Sua senha deve ter ao menos 8 caracteres, uma letra um número e uma letra maiúscula!"
 }
 
 export const noMovement = {
@@ -44,4 +40,32 @@ export const transactionError = {
 
 export const passwordInvalidConfirm = {
     message: "As senhas não batem"
+}
+
+export const usernameAndPasswordRequired = {
+    message: "Usuário e Senha campos obrigatórios"
+}
+
+export const sessionExpired = {
+    message: "Usuário e Senha campos obrigatórios"
+}
+
+export const shotValue = {
+    message: "Selecione um valor para transação maior que 0!"
+}
+
+export const userSelctedNotExist = {
+    message: "O usuário selecionado não existe"
+}
+
+export const passwordInvalid = {
+    message: "Senha Inválida"
+}
+
+export const balanceShot ={
+    message: "Você não tem saldo suficiente para realizar esta transação, por favor verifique o seu saldo!"
+}
+
+export const invalidTransactionAccount ={
+    message: "Você não pode realizar uma transação para sua própria conta!"
 }
