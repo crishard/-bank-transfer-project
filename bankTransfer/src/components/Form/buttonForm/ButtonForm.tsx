0899fdece0f031906272424fe83640cfde8d4fb7
crishard <crislantorres9@gmail.com>
2023-03-30 15:56:53 -0300

    refactoring: button form

--- bankTransfer/src/components/Form/buttonForm/ButtonForm.tsx
+++ bankTransfer/src/components/Form/buttonForm/ButtonForm.tsx
@@ -1,11 +1,13 @@
 import "./ButtonForm.css"
 
-interface IButtonProps{
+interface IButtonProps {
     text: string;
 }
 
-export function ButtonForm(props: IButtonProps){
-    return(
-        <button type="submit" className="button-form bg-indigo-600 py-2.5 px-4 text-sm font-medium text-white" >{props.text}</button>
+export function ButtonForm(props: IButtonProps) {
+    return (
+        <button type="submit" className="button-form" >
+            {props.text}
+        </button>
     );
 }
