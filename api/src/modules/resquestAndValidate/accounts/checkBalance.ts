6d8bc67177304791ead7da6fc6bebcdb9b5f465c
crishard <crislantorres9@gmail.com>
2023-03-30 13:18:08 -0300

    refactoring: isolando responsabilidade de checagem de saldo

---
+++ api/src/modules/resquestAndValidate/accounts/checkBalance.ts
@@ -0,0 +1,24 @@
+import { prisma } from "../../../dataBase/prismaClient";
+
+export async function checkBalance(userId: string, value: number) {
+
+    const findUser = await prisma.users.findUnique({
+        where: {
+            id: userId
+        }
+    });
+
+    const findAccount = await prisma.accounts.findUnique({
+        where: {
+            id: findUser?.accountId
+        }
+    });
+
+    if(findAccount){
+        if(findAccount.balance != null  && findAccount.balance < value){
+            return false
+        } else{
+            return true;
+        }
+    }
+}
