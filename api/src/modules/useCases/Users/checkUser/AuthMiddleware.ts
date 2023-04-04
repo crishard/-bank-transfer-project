d17dd94176ea561a7a9b131af7900e38b683baef
crishard <crislantorres9@gmail.com>
2023-04-04 09:11:29 -0300

    refactoring: correção simples de midlleware

--- api/src/modules/useCases/Users/checkUser/AuthMiddleware.ts
+++ api/src/modules/useCases/Users/checkUser/AuthMiddleware.ts
@@ -8,13 +8,6 @@ interface IPayload {
 export class AuthMiddleware {
   public static async authenticate(req: Request, res: Response, next: NextFunction) {
 
-    const jwtSecret = process.env.JWT_SECRET;
-    if (!jwtSecret) {
-      console.error('A chave secreta JWT não foi definida nas variáveis de ambiente!');
-      process.exit(1);
-    }
-
-
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       return res.status(401).json({ message: "Token não existe" });
@@ -23,7 +16,7 @@ export class AuthMiddleware {
     const [, token] = authHeader.split(" ");
 
     try {
-      const { sub } = verify(token, jwtSecret) as IPayload;
+      const { sub } = verify(token, "chavesecreta") as IPayload;
       req.userId = sub;
       return next();
     } catch (err) {
