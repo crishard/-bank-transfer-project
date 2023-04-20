2eb25afc5c1cc78575fe6077a7af9d3565f365a6
crishard <crislantorres9@gmail.com>
2023-04-20 09:46:58 -0300

    refactoring: refatoração do formulário de login e registro

--- bankTransfer/src/components/Form/Form.tsx
+++ bankTransfer/src/components/Form/Form.tsx
@@ -12,9 +12,7 @@ import { userRegister } from "../../services/register";
 import { useNavigate } from "react-router-dom";
 import { login } from "../../services/login";
 
-
 type FormData = yup.InferType<typeof UserValidate>;
-
 
 interface IFormProps {
   register: boolean;
@@ -23,7 +21,6 @@ interface IFormProps {
 }
 
 export function Form(props: IFormProps) {
-
 
   const [isLoaded, setIsLoaded] = useState(false);
   const [reqLoaded, setReqLoaded] = useState(false);
@@ -64,7 +61,6 @@ export function Form(props: IFormProps) {
     } catch (error) {
       setError("Ocorreu um erro. Tente novamente mais tarde.");
     }
-
     setReqLoaded(false);
   }
 
@@ -74,32 +70,29 @@ export function Form(props: IFormProps) {
     }, 3000);
   }, []);
 
-
   return (
-
     <>
-      {isLoaded ? <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 main-form" >
+      {isLoaded ?
+        <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 main-form" >
+          {reqLoaded && <div className="absolute h-full w-full bg-[#efefef]"><Loader /></div>}
+          <div className="px-8 py-8 container-form">
+            <HeaderForm title={props.titleHeader} option={props.optionHeader} hrefProp={props.register} />
 
+            <form className="w-full mt-8 form text-center" onSubmit={handleSubmit(onSubmit)}>
+              <input onFocus={() => setError("")} className="input-form " autoComplete="off" placeholder='Username' type='string' {...register("username")} />
+              <MessageError text={errors.username?.message} />
 
-        {reqLoaded && <div className="absolute h-full w-full bg-[#efefef]"><Loader /></div>}
-        <div className="px-8 py-8 container-form">
-          <HeaderForm title={props.titleHeader} option={props.optionHeader} hrefProp={props.register} />
+              <input onFocus={() => setError("")} className="input-form " autoComplete="off" placeholder='Password' type='password' {...register("password")} />
+              <MessageError text={errors.password?.message} />
+              {error && <MessageError text={error} />}
 
-          <form className="w-full mt-8 form text-center" onSubmit={handleSubmit(onSubmit)}>
-            <input onFocus={() => setError("")} className="input-form " autoComplete="off" placeholder='Username' type='string' {...register("username")} />
-            <MessageError text={errors.username?.message} />
+              {
+                props.register ? <ButtonForm text="Enviar" /> : <ButtonForm text="Entrar" />
+              }
 
-            <input onFocus={() => setError("")} className="input-form " autoComplete="off" placeholder='Password' type='password' {...register("password")} />
-            <MessageError text={errors.password?.message} />
-            {error && <MessageError text={error} />}
-
-            {
-              props.register ? <ButtonForm text="Enviar" /> : <ButtonForm text="Entrar" />
-            }
-
-          </form>
-        </div>
-      </div > : <Loader />}
+            </form>
+          </div>
+        </div > : <Loader />}
     </>
   );
 }
